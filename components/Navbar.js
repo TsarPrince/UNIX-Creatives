86b447d873784190c043a7ababbce5ab5d4c305a
Abhinay Pandey <abhinaypandey02@gmail.com>
2022-06-30 22:21:35 +0530

    making nav bar static

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,21 +6,7 @@ export const Navbar = () => {
   // on mobile view
   const showHiddenNavbarContent = () => {
     document.querySelector('.navbar-hidden').classList.toggle('hidden');
-  }
 
-  // typeof window === undefined on server side
-  if (typeof window !== 'undefined') {
-    const navbar = document.querySelector('#navbar');
-    let prevScrollTop = 0;
-    document.addEventListener('scroll', () => {
-      let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-      if (scrollTop > prevScrollTop) {
-        navbar.style.top = `-${navbar.clientHeight}px`;
-      } else {
-        navbar.style.top = 0;
-      }
-      prevScrollTop = scrollTop;
-    })
   }
 
   const handleClick = (event) => {
@@ -28,6 +14,8 @@ export const Navbar = () => {
     const current = event.target;
     prev.className = 'nav-inactive text-slate-600 hover:text-slate-500';
     current.className = 'nav-active text-blue-500 hover:text-blue-600';
+    document.querySelector('.navbar-hidden').classList.add('hidden');
+
   }
 
   return (
@@ -37,10 +25,10 @@ export const Navbar = () => {
           <Image src='/logo.svg' width={127} height={24} alt='UNIX creatives logo'></Image>
         </div>
         <div className='navbar-hidden hidden bg-white py-4 text-lg font-semibold absolute top-full inset-x-0 flex flex-col items-center space-y-4 w-full shadow-md lg:relative lg:shadow-none lg:flex-row lg:justify-center lg:w-auto lg:space-y-0 lg:block lg:bg-transparent lg:space-x-12'>
-          <Link href='/#home'><a className='nav-active text-blue-500 hover:text-blue-600' onClick={handleClick}>Home</a></Link>
-          <Link href='/#services'><a className='nav-inactive text-slate-600 hover:text-slate-500' onClick={handleClick}>Services</a></Link>
-          <Link href='/#our-work'><a className='nav-inactive text-slate-600 hover:text-slate-500' onClick={handleClick}>Our Work</a></Link>
-          <Link href='/#pricing'><a className='nav-inactive text-slate-600 hover:text-slate-500' onClick={handleClick}>Pricing</a></Link>
+          <a href='#home'><span className='nav-active text-blue-500 hover:text-blue-600' onClick={handleClick}>Home</span></a>
+          <a href='#services'><span className='nav-inactive text-slate-600 hover:text-slate-500' onClick={handleClick}>Services</span></a>
+          <a href='/#our-work'><span className='nav-inactive text-slate-600 hover:text-slate-500' onClick={handleClick}>Our Work</span></a>
+          <a href='/#pricing'><span className='nav-inactive text-slate-600 hover:text-slate-500' onClick={handleClick}>Pricing</span></a>
         </div>
         <div className='hidden lg:block'>
           <button className='btn-rounded-md'>
